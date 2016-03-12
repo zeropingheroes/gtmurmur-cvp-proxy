1660e59c8ec8487c00c9c604eea48e0d33e63a35
root <root@zeropingheroes.co.uk>
2016-03-12 05:11:47 +0000

    Updated port number

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run -d -p 8080:80 --name gtmurmur-cvp-proxy_1 -v /var/www/gtmurmur-cvp-proxy/src:/var/www/html/ gtmurmur-cvp-proxy
+docker run -d -p 64739:80 --name gtmurmur-cvp-proxy_1 -v /var/www/gtmurmur-cvp-proxy/src:/var/www/html/ gtmurmur-cvp-proxy
+
